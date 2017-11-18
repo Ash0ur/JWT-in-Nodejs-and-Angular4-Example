c4865524d57f6a3d3090980b7ecde83fb275eb2d
Ash0ur <codeblooms.gmail.com>
2017-11-18 17:21:26 +0200

    a commit

--- Github/server/models/user.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-
-UserSchema = mongoose.Schema({
-    email:{type:String,required:true},
-    password:{type:String,required:true}
-});
-
-module.exports = mongoose.model('User', UserSchema,'users');
